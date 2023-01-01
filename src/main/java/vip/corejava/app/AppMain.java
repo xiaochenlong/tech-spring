5f51e0923fe403e0cc3ad02ccd0c84471080cee3
xcl <xcl@xcl.local>
2023-01-01 22:00:59 +0800

    rm

--- src/main/java/vip/corejava/app/AppMain.java
+++ src/main/java/vip/corejava/app/AppMain.java
@@ -1,15 +1,8 @@
 package vip.corejava.app;
 
-import org.springframework.beans.factory.BeanFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.event.SimpleApplicationEventMulticaster;
-import org.springframework.context.support.AbstractApplicationContext;
-import org.springframework.core.task.SimpleAsyncTaskExecutor;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
  * @author xcl
